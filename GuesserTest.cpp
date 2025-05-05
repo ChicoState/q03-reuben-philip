f96b85d7bb5bcff42e7249a4342c894c95949b70
reuben-philip <reubenaphilip@gmail.com>
2025-05-05 03:23:10 -0700

    update GuesserTest.cpp

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,52 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+TEST(GuesserTest, passwords_match){
+	Guesser g("test");
+	ASSERT_TRUE(g.match("test"));
 }
+
+TEST(GuesserTest, wrong_guess){
+	Guesser g("test");
+	ASSERT_FALSE("tesa");
+}
+
+TEST(GuesserTest, wrong_guess_increments_down){
+	Guesser g("test");
+	g.match("tesa");
+	ASSERT_EQ(g.remaining(),2);
+}
+
+TEST(GuesserTest, cant_use_word_after_3_guesses){
+	Guesser g("test");
+	g.match("tesa");
+	g.match("tesb");
+	g.match("tesc");
+	ASSERT_FALSE(g.match("test"));
+}
+
+TEST(GuesserTest,remaining_resets){
+	Guesser g("test");
+	g.match("tesa");
+	g.match("tesb");
+	g.match("test");
+
+	ASSERT_EQ(g.remaining(),3);
+}
+
+TEST(GuesserTest, brute_force_lock){
+	Guesser g("test");
+	ASSERT_FALSE(g.match("csuchico"));
+	ASSERT_FALSE(g.match("test"));
+}
+
+TEST(GuesserTest, calculate_distance){
+	Guesser g("test");
+	ASSERT_EQ(g.distance("test"),0);
+	ASSERT_EQ(g.distance("tesa"),1);
+	ASSERT_EQ(g.distance("tes"),1);
+	ASSERT_EQ(g.distance("teaa"),2);
+	ASSERT_EQ(g.distance("te"),2);
+}
+
+
